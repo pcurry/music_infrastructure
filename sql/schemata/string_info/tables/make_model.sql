582f8412db8104b994d1c27b7520dcff98f1c689
Paul Curry <pcurry@gmail.com>
2011-02-18 14:28:44 -0500

    Started filling out string info because I got packs of strings.

---
+++ sql/schemata/string_info/tables/make_model.sql
@@ -0,0 +1,17 @@
+﻿-- Table: string_info.make_model
+
+-- DROP TABLE string_info.make_model;
+
+CREATE TABLE string_info.make_model
+(
+  sid serial NOT NULL,
+  brand_sid integer,
+  CONSTRAINT make_model_pkey PRIMARY KEY (sid),
+  CONSTRAINT brand_sid_fkey FOREIGN KEY (brand_sid)
+      REFERENCES brand_info.brand (sid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE string_info.make_model OWNER TO music_infra_owner;
---
+++ sql/schemata/string_info/tables/string_diameter.sql
@@ -0,0 +1,15 @@
+﻿-- Table: string_info.string_diameter
+
+-- DROP TABLE string_info.string_diameter;
+
+CREATE TABLE string_info.string_diameter
+(
+  sid serial NOT NULL,
+  in_inches numeric(8,6) NOT NULL,
+  in_mm numeric(10,6),
+  CONSTRAINT string_diameter_pkey PRIMARY KEY (sid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE string_info.string_diameter OWNER TO music_infra_owner;
