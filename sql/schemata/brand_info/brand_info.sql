2be594405094d30d270cfe8b3c48c9cde8c3fa72
Paul Curry <pcurry@gmail.com>
2011-04-06 18:28:42 -0400

    The two other main schemata for the next bit of development.

---
+++ sql/schemata/brand_info/brand_info.sql
@@ -0,0 +1,7 @@
+-- Schema: brand_info
+
+-- DROP SCHEMA brand_info
+
+CREATE SCHEMA brand_info
+  AUTHORIZATION music_infra_owner;
+
---
+++ sql/schemata/instrument_info/instrument_info.sql
@@ -0,0 +1,7 @@
+-- Schema: instrument_info
+
+-- DROP SCHEMA instrument_info
+
+CREATE SCHEMA instrument_info
+  AUTHORIZATION music_infra_owner;
+
