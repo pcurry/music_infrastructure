5cdfa88be94e9b1d95ada9df0e28f89ee97dd992
Paul Curry <pcurry@gmail.com>
2011-04-06 19:40:58 -0400

    Fixed name references in strings.sql.

--- sql/schemata/parts_etc/tables/strings.sql
+++ sql/schemata/parts_etc/tables/strings.sql
@@ -6,9 +6,9 @@ CREATE TABLE parts_etc.strings
 (
   sid serial NOT NULL,
   string_diameter_sid INTEGER NOT NULL, 
-  string_ends_sid INTEGER NOT NULL, 
+  string_end_sid INTEGER NOT NULL, 
   material_sid INTEGER, 
-  string_coatings_sid INTEGER,
+  string_coating_sid INTEGER,
   CONSTRAINT strings_table_pkey PRIMARY KEY (sid)
 )
 WITH (
